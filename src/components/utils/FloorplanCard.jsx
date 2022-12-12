5884593e25e69e9fc935c82b7651e3b6f763bf8a
solovyovsdom <joelborofsky@mac.com>
2022-12-12 16:38:20 -0600

    began work on modal

--- src/components/utils/FloorplanCard.jsx
+++ src/components/utils/FloorplanCard.jsx
@@ -1,15 +1,29 @@
 import { useState } from 'react'
 import { GiExpand, GiBed, GiBathtub, GiVideoCamera } from 'react-icons/gi'
 import { BsCurrencyDollar } from 'react-icons/bs'
+import FloorplanModal from './FloorplanModal'
 
 function FloorplanCard(props) {
-const { imageUrl, title, size, bedrooms, bathrooms, rent, tourUrl} = props
+  const { imageUrl, title, size, bedrooms, bathrooms, rent, tourUrl } = props
+  const [floorplanModal, setfloorplanModal] = useState(false)
+
+  function openModal() {
+    console.log(props.id)
+    setfloorplanModal(!floorplanModal)
+  }
+
   return (
     <div className='Floorplans__card'>
       <img
         src={imageUrl}
         alt={title}
+        onClick={openModal}
         className='Floorplans__card--img'
+      />
+      <FloorplanModal
+        props={props}
+        toggle={floorplanModal}
+        action={openModal}
       />
       <h3 className='Floorplans__card--title'>{title}</h3>
       <p className='Floorplans__card--card-content'>
@@ -17,10 +31,12 @@ const { imageUrl, title, size, bedrooms, bathrooms, rent, tourUrl} = props
         {size} SQ.FT
       </p>
       <p className='Floorplans__card--card-content'>
-        <GiBed />{bedrooms} Bedroom
+        <GiBed />
+        {bedrooms} Bedroom
       </p>
       <p className='Floorplans__card--card-content'>
-        <GiBathtub />{bathrooms} Bathroom
+        <GiBathtub />
+        {bathrooms} Bathroom
       </p>
       <p className='Floorplans__card--card-content'>
         <BsCurrencyDollar />
--- src/components/utils/FloorplanModal.jsx
+++ src/components/utils/FloorplanModal.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { GrClose } from 'react-icons/gr'
 
-function FloorplanModal() {
+function FloorplanModal(props) {
+  const { imageUrl, toggle, action } = props
+  const modalState = toggle
+
   return (
-    <div>FloorplanModal</div>
+    <div className={`Modal-container ${modalState ? 'active' : ''}`}>
+      <div className="Modal-container__modal">
+        <img src={imageUrl} alt="" className="Modal-container__modal--image" />
+
+      </div>
+    </div>
   )
 }
 
