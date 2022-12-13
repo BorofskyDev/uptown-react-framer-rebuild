d5fb27da2d942f3c44b205f6b1b1184d7a2a9b5e
solovyovsdom <joelborofsky@mac.com>
2022-12-13 14:36:46 -0600

    got floorplan images to map correctly

--- src/components/Floorplans.jsx
+++ src/components/Floorplans.jsx
@@ -3,28 +3,28 @@ import divider from '../assets/divider.webp'
 import data from '../assets/floorplanData'
 import { useState } from 'react'
 
-
 export default function Floorplans() {
-  const [floorplanModal, setFloorplanModal] = useState(false);
+  const [floorplanModal, setFloorplanModal] = useState(false)
   const cards = data.map((item) => {
-    return <FloorplanCard key={item.id} {...item} openModal={openModal} />
+    return (
+      <div>
+        <FloorplanCard key={item.id} {...item} openModal={openModal} />
+        <FloorplanModal action={openModal} toggle={floorplanModal} {...item} />
+      </div>
+    )
   })
 
   function openModal() {
-    console.log(cards.id)
     setFloorplanModal(!floorplanModal)
   }
 
   return (
     <section className='Floorplans section-container' id='floorplans'>
-      <img src={divider} alt='' className='divider'/>
+      <img src={divider} alt='' className='divider' />
       <div className='title'>Floorplans</div>
       <div className='Floorplans__card-container'>
         {cards}
-        <FloorplanModal
-          action={openModal}
-          toggle={floorplanModal}
-        />
+       
       </div>
       <p className='Floorplans__tag'>
         Availability of our apartment homes can change from day to day, please{' '}
--- src/components/utils/FloorplanModal.jsx
+++ src/components/utils/FloorplanModal.jsx
@@ -4,11 +4,12 @@ import flr from '../../assets/apt-imgs/1bdr/1-bedroom-floorplan.webp'
 function FloorplanModal(props) {
   const { imageUrl, toggle, action } = props
   const modalState = toggle
+  console.log(imageUrl)
 
   return (
     <div className={`Modal-container ${modalState ? 'active' : ''}`}>
       <div className="Modal-container__modal">
-        <img src={flr} alt="" className="Modal-container__modal--image" />
+        <img src={imageUrl} alt="" className="Modal-container__modal--image" />
 
       </div>
     </div>
